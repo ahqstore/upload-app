7e759dfdc224b38aaea2d5b616f0c98425e91c25
AHQ Miness <ahqsecret@gmail.com>
2024-03-10 17:43:51 +0530

    Stuff

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from './main'
+import { promisify } from 'node:util'
+import { exec } from 'child_process'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+import { context, getOctokit } from '@actions/github'
+import core from '@actions/core'
+
+const Exec = promisify(exec)
+
+;(async () => {
+  const { stdout } = await Exec('npm i -g @ahqstore/cli')
+
+  console.log(stdout)
+
+  const { owner, repo } = context.repo
+
+  const rep = `${owner}/${repo}`
+
+  const r_id = core.getInput('release')
+
+  if (!process.env['GH_TOKEN']) {
+    core.error('GH_TOKEN environment variable is not set')
+  }
+
+  const { stdout: out } = await Exec('ahqstore build', {
+    env: {
+      ...process.env,
+      RELEASE_ID: r_id,
+      GITHUB_REPOSITORY: rep,
+      GH_ACTION: 'true'
+    }
+  })
+
+  const [k, value] = out.trim().split('=')
+
+  core.info(`Got "${k}" as "${value}"`)
+
+  const upload = core.getBooleanInput('upload')
+
+  if (upload) {
+    await getOctokit(process.env['GH_TOKEN']!).rest.issues.createComment({
+      owner: 'ahqstore',
+      repo: 'apps',
+      issue_number: 3,
+      body: `/store set ${value}`
+    })
+  }
+})()
